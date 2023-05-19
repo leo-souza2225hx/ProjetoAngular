7775b61c4b96e0deea8598a58f2cf19dc1852220
leo-souza2225hx <132064038+leo-souza2225hx@users.noreply.github.com>
2023-05-19 16:14:52 +0000

    Implementação da estrutura

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,21 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule,CommonModule],
 })
 export class HomePage {
-  constructor() {}
+ calendario = {
+  mes:'Maio',
+  ano: 2023,
+  foto: 'https://images.pexels.com/photos/15499727/pexels-photo-15499727/free-photo-of-baloes-bexigas-globos-ceu-azul.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  dias_da_semana:['DOM','SEG','TER','QUA','QUI','SEX','SAB'],
+  primeira_semana: [30, 1, 2, 3, 4, 5, 6],
+  segunda_semana:[7, 8, 9, 10, 11, 12, 13],
+  terceira_semana:[14, 15, 16, 17, 18, 19, 20],
+  quarta_semana:[21, 22, 23, 24, 25, 26, 27],
+  quinta_semana:[28, 29, 30, 31]
+  
+
+  
+ }
 }
