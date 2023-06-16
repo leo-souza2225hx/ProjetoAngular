075e01aaa95aee7b1ba310f7dc6cdbfe10f74fe1
leo-souza2225hx <132064038+leo-souza2225hx@users.noreply.github.com>
2023-06-16 15:54:51 +0000

    implementação

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -7,30 +8,42 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule,CommonModule],
+  imports: [IonicModule,CommonModule,FormsModule],
 })
 export class HomePage {
+  
+  //valores
+
  valor1:any = null;
  valor2:any = null;
  resultado:any=null;
  operador:any=null;
+
+etapa1:any=true
   
   exibir(numero:any) {
-
+ 
    if(this.valor1==null){
     this.valor1 = numero
+    this.resultado = numero
    }else{
     if(this.valor2==null){
       this.valor2=numero;
+      this.resultado = numero
     }
    }
-  
+  //mostrar no console
 		console.log(this.valor1);
     console.log(this.operador);
       console.log(this.valor2);
 	}
-   
+
   operacao(operador:any){
+    this.operador=operador
+  }
+   
+  calcular(){
+    //operações logicas
     if(this.operador=='+'){
       this.resultado=this.valor1 + this.valor2;
     }
@@ -46,5 +59,25 @@ export class HomePage {
     console.log(this.operador);
   }
 
-  
+  deleteDigit() {
+   if (this.valor1 != null && this.valor2==null){
+      this.valor1=null
+      this.resultado=null
+   }
+   if (this.valor1 != null && this.valor2 !=null){
+    this.valor2=null
+    this.resultado=null
+ }
+  }
+  clear(){
+    this.valor1=null
+    this.valor2=null
+    this.resultado=null
+    this.operador=null
+  }
+
 }
+
+
+
+
