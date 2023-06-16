c409fef7b7ee1cec1faea02ec9275ea33f498ae4
leo-souza2225hx <132064038+leo-souza2225hx@users.noreply.github.com>
2023-06-16 13:49:26 +0000

    complementos(60%)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,13 +10,41 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule,CommonModule],
 })
 export class HomePage {
+ valor1:any = null;
+ valor2:any = null;
+ resultado:any=null;
+ operador:any=null;
   
- 
-  
-  exibir(numero: number) {
+  exibir(numero:any) {
 
-		console.log(numero);
+   if(this.valor1==null){
+    this.valor1 = numero
+   }else{
+    if(this.valor2==null){
+      this.valor2=numero;
+    }
+   }
+  
+		console.log(this.valor1);
+    console.log(this.operador);
+      console.log(this.valor2);
 	}
-  
+   
+  operacao(operador:any){
+    if(this.operador=='+'){
+      this.resultado=this.valor1 + this.valor2;
+    }
+    else if(this.operador=='-'){
+      this.resultado= this.valor1 - this.valor2;
+    }
+    else if(this.operador=='*'){
+      this.resultado = this.valor1 * this.valor2;
+    }
+    else if(this.operador=='/'){
+      this.resultado = this.valor1 / this.valor2;
+    }
+    console.log(this.operador);
+  }
 
+  
 }
